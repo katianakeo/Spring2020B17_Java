27a934755673b77fb5d78b154b41ff9af5f53973
katianakeo <katiana.keopradit@yahoo.com>
2020-04-27 10:31:45 -0400

    Day29 Class

---
+++ src/day29_Wrapper_ArrayList/Wrapper.java
@@ -0,0 +1,23 @@
+package day29_Wrapper_ArrayList;
+
+public class Wrapper {
+    public static void main(String[] args) {
+        int a = 100;  // primitive
+        Integer b = a; // wrapper class
+
+        short c = 200;
+        int c1 = c;
+        Integer N1 = c1; // integer can only be assigned to int
+
+        int a1 = 20;
+        float f1 = a1;
+
+        Float f2 = f1;
+
+        Byte b1 = 126;
+        int a3 = b1;
+
+
+
+    }
+}
