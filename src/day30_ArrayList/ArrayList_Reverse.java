c85aadd05a60f130df5a83740d4282be4ec47dd9
katianakeo <katiana.keopradit@yahoo.com>
2020-04-28 11:28:36 -0400

    Day30 Class

--- src/day30_ArrayList/ArrayList_Reverse.java
+++ src/day30_ArrayList/ArrayList_Reverse.java
@@ -1,4 +1,24 @@
 package day30_ArrayList;
 
+import java.util.ArrayList;
+
 public class ArrayList_Reverse {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+
+        for(int i=1; i< 6; i++){
+            list.add(i);
+        }
+        System.out.println(list);
+
+
+        ArrayList<Integer> reverseList = new ArrayList<>();
+        for(int i = list.size()-1; i>=0; i--){
+            int num = list.get(i);
+            reverseList.add(num);
+        }
+        System.out.println(reverseList);
+    }
+
 }
