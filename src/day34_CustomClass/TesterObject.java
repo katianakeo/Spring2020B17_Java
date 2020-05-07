e56bc53967081ef3756ed871de6489a94cf84b95
katianakeo <katiana.keopradit@yahoo.com>
2020-05-07 11:56:42 -0400

    Day34 Class

---
+++ src/day34_CustomClass/TesterObject.java
@@ -0,0 +1,8 @@
+package day34_CustomClass;
+
+public class TesterObject {
+    public static void main(String[] args) {
+        Tester tester1 = new Tester();
+        tester1.setTesterInfo("Katiana", 7347947, "Tester", 60);
+    }
+}
