456c0c4b8b50e70a428dc7c4c7e9ea756bf43d71
katianakeo <katiana.keopradit@yahoo.com>
2020-05-08 10:17:43 -0400

    Day36 Class

--- src/day36_StaticBlock/StaticBlock.java
+++ src/day36_StaticBlock/StaticBlock.java
@@ -1,4 +1,17 @@
 package day36_StaticBlock;
 
 public class StaticBlock {
+    public static void main(String[] args) {
+        System.out.println("main method");
+    }
+
+    static{
+        System.out.println("static method");
+    }
+
+
+
+    public static void method1(){
+        System.out.println("custom method");
+    }
 }
