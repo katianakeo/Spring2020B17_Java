4c70a40e92d963d0753b5ffb4996e1fc113557a6
katianakeo <katiana.keopradit@yahoo.com>
2020-05-11 11:45:33 -0400

    Day37 Class

---
+++ src/day37_Constructors/Data.java
@@ -0,0 +1,14 @@
+package day37_Constructors;
+
+public class Data {
+    static int staticVariable = 300;
+    int instanceVariable = 400;
+
+    public static void staticMethod(){
+        System.out.println("Static Method");
+    }
+
+    public void instanceMethod(){
+        System.out.println("Instance Method");
+    }
+}
